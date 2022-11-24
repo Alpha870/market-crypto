dc144d2658d607fd8cbac2cbf694e7fb4211c3aa
Omar_Perez <omar1987perez2@gmail.com>
2022-11-24 19:58:33 +0100

    añadiendo la petición a la API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const [stateData, setStateData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coincap.io/v2/assets")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setStateData(data.data);
+      })
+      .catch(() => {
+        console.error("La petición a fallado...");
+      });
+  }, []);
 
   return (
-      <h1>Vite + React</h1>
-  )
+    <>
+      <h1>Lista de criptomonedas</h1>
+      <ol>
+        {stateData.map(({ name, priceUsd }) => (
+          <li>
+            Nombre: {name}
+            Precio: {priceUsd}
+          </li>
+        ))}
+      </ol>
+    </>
+  );
 }
 
-export default App
+export default App;
