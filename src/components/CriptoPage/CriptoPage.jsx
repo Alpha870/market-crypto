1ac137c0e55e57db991c4d539d8ff0c4c9d30179
Omar_Perez <omar1987perez2@gmail.com>
2022-11-27 11:21:04 +0100

    Añado 2 llamadas a las api para extraer info dentro de criptopage

--- src/components/CriptoPage/CriptoPage.jsx
+++ src/components/CriptoPage/CriptoPage.jsx
@@ -4,10 +4,11 @@ import { useParams } from "react-router-dom";
 import "./CriptoPage.css";
 
 const CriptoPage = () => {
-  const [dataPage, setDataPage] = useState([]);
-
   const API_URL = import.meta.env.VITE_API_URL;
   const params = useParams();
+
+  const [dataPage, setDataPage] = useState({});
+  const [dataHistory, setDataHistory] = useState([]);
 
   useEffect(() => {
     axios
@@ -15,15 +16,61 @@ const CriptoPage = () => {
       .then((resp) => {
         setDataPage(resp.data.data);
       })
-
-      .catch(() => {
-        console.error("La peticion falló");
-      });
+      .catch((e) => console.error(e));
   }, []);
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}assets/${params.id}/history?interval=d1`)
+      .then((resp) => {
+        setDataHistory(resp.data.data);
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
+  if (!dataPage) return <h3>Cargando...</h3>;
 
   return (
     <>
-      <h1>Soy una criptomonedas {params.id}</h1>
+      <section className="page-cripto">
+        <div className="info">
+          <p>
+            <span className="label">Nombre: </span>
+            {dataPage.name}
+          </p>
+          <p>
+            <span className="label">Código: </span>
+            {dataPage.symbol}
+          </p>
+          <p>
+            <span className="label">Precio: </span>
+            {parseFloat(dataPage.priceUsd).toFixed(3)}
+          </p>
+          <p>
+            <span className="label">Supply: </span>
+            {parseFloat(dataPage.supply).toFixed(0)}
+          </p>
+          <p>
+            <span className="label">Max.supply: </span>
+            {parseFloat(dataPage.maxSupply).toFixed(0)}
+          </p>
+
+          <p>
+            <a className="label">explorador: </a>
+            {dataPage.explorer}
+          </p>
+        </div>
+
+        <div className="history-cuadricula">
+          <h1>HISTORIAL</h1>
+          {dataHistory.map(({ time, priceUsd, date }) => (
+            <div key={time} className="tabla-history">
+              <p>{date.split("T")[0]}</p>
+              <p className="h1-page">{parseFloat(priceUsd).toFixed(2)}</p>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 };
